6f7c19682c700fbfe1f83deadc2e399f7b4bbcc1
markzz <stevenmaithya42@gmail.com>
2026-04-09 01:48:04 +0300

    open CORS for production

--- backend/src/Config/cors.config.js
+++ backend/src/Config/cors.config.js
@@ -1,22 +1,6 @@
 const cors = require("cors");
 
-let allowedOrigins = ["http://localhost:5173"];
-
-try {
-  if (process.env.CORS_ORIGIN) {
-    allowedOrigins = JSON.parse(process.env.CORS_ORIGIN);
-  }
-} catch (error) {
-  console.error("Invalid CORS_ORIGIN in .env. Using default localhost:5173");
-}
-
 module.exports = cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
+  origin: "*",
+  credentials: false,
 });
