ca1759e053b1ef9bc661e04b4988aa7765e4f543
markzz <stevenmaithya42@gmail.com>
2026-04-09 01:23:57 +0300

    fix production API URL to use env variable

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,12 +1,10 @@
 const config_urls = {
   local: 'http://localhost:3000',
   development: 'http://localhost:8000',
-  // production: import.meta.env.VITE_SERVER_URL_API,
-  production2: 'https://tutuion-management-backend.vercel.app',
-  production: 'https://tutuion-management.onrender.com',
+  production: import.meta.env.VITE_SERVER_URL_API || 'http://localhost:8000',
 };
 
 export const BASE_URL = config_urls[import.meta.env.VITE_DEVELOPMENT_MODE || 'development'];
 export const API_URL =
   config_urls[import.meta.env.VITE_DEVELOPMENT_MODE || 'development'] + '/api/v1';
-export const public_url = 'https://www.Summit Scholars Hubtutorial.com';
+export const public_url = 'https://www.summitscholars.co.ke';
